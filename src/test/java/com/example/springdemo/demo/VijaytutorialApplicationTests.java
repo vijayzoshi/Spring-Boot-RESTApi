1a1bdf927fbdade0afa5317833d2a46a345fb7d2
vijayzoshi <zoshivj@gmail.com>
2025-02-08 15:19:07 +0530

    v2

---
+++ src/test/java/com/example/springdemo/demo/VijaytutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VijaytutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
